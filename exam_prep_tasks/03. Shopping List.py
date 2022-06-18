88946f4186fdc55c705d811fc04b7864c67dfa35
Ivelin Borisov <90949939+borisov5@users.noreply.github.com>
2022-06-18 22:45:59 +0300

    Add files via upload

---
+++ exam_prep_tasks/03. Shopping List.py
@@ -0,0 +1,24 @@
+def shopping_list(budget, **kwargs):
+    # product and value(product price, quantity)
+    return kwargs
+
+print(shopping_list(100,
+                    microwave=(70, 2),
+                    skirts=(15, 4),
+                    coffee=(1.50, 10),
+                    ))
+
+print(shopping_list(20,
+                    jeans=(19.99, 1),
+                    ))
+
+print(shopping_list(104,
+                    cola=(1.20, 2),
+                    candies=(0.25, 15),
+                    bread=(1.80, 1),
+                    pie=(10.50, 5),
+                    tomatoes=(4.20, 1),
+                    milk=(2.50, 2),
+                    juice=(2, 3),
+                    eggs=(3, 1),
+                    ))
