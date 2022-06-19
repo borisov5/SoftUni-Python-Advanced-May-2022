059b6c1fde3fbd66e9c5828f23f1581c0756e8ae
Ivelin Borisov <90949939+borisov5@users.noreply.github.com>
2022-06-19 11:15:18 +0300

    Add files via upload

--- exam_prep_tasks/03. Shopping List.py
+++ exam_prep_tasks/03. Shopping List.py
@@ -1,6 +1,21 @@
 def shopping_list(budget, **kwargs):
-    # product and value(product price, quantity)
-    return kwargs
+    if budget < 100:
+        return 'You do not have enough budget.'
+
+    result = ''
+    product_counter = 0
+    for product, value in kwargs.items():
+        product_price = value[0]
+        quantity = value[1]
+        product_cost = product_price * quantity
+        if product_cost <= budget:
+            result += f'You bought {product} for {product_cost:.2f} leva.\n'
+            budget -= product_cost
+            product_counter += 1
+            if product_counter == 5:
+                break
+    return result
+
 
 print(shopping_list(100,
                     microwave=(70, 2),
